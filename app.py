dc99c5d5c80b73ea953f0538b3ce3fc8acc335ad
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2025-03-30 00:21:25 -0400

    New pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,27 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World</h1>"
 
+@app.route('/hello')
+def hello():
+   return 'Hello World'
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"hello {name}"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f'{number1} + {number2} = {number1 + number2}'
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args['greeting'] 
+        name = request.args.get('name')
+        return f'{greeting}, {name}'
+    else:
+        return 'Some Parameters are missing'
 
 
 if __name__ == '__main__':
