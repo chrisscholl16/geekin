1ff67cca843bf6bdfba1d704489b1f1b548ae860
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2025-04-01 13:18:56 -0400

    Templates 1

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
 
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
-
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-   if request.method == 'GET':
-       return 'You made a GET request\n'
-   elif request.method =='POST':
-       return 'You made a POST request\n'
-   else:
-       return 'You will never see this message\n'
-
-
-@app.route('/greet/<name>')
-def greet(name):
-    return f"hello {name}"
-
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1} + {number2} = {number1 + number2}'
-
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args['greeting'] 
-        name = request.args.get('name')
-        return f'{greeting}, {name}'
-    else:
-        return 'Some Parameters are missing'
+    myList = [10,20,30,40]
+    return render_template('index.html', myList=myList)
 
 
 if __name__ == '__main__':
