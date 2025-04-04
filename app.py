deaf03c28626e56edf5914566a0020810c7b2397
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2025-04-03 20:58:04 -0400

    forms

--- app.py
+++ app.py
@@ -1,34 +1,26 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    myList = [10,20,30,40]
-    return render_template('index.html', myList=myList)
+    if request.method == "GET":
+       return render_template('index.html')
+    elif request.method =="POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-@app.route('/other')
-def other():
-    some_text = "Some text"
-    return render_template('other.html', some_text=some_text)
+        if username == 'cscholl' and password == 'password':
+            return 'Success'
+        else:
+            return 'Failure'
+        
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    return 
 
-@app.route('/redirect_endpoint')
-def redirect_endpoint():
-     return redirect(url_for('other'))
 
-@app.template_filter('reverse_string')
-def reverse_string(s):
-    return s[::-1]
-
-@app.template_filter('repeat')
-def repeat(s, times=2):
-    return s * times
-    
-
-@app.template_filter('alternate_case')
-def alternate_case(s):
-        return ''.join([c.upper() if i % 2  == 0 else c.lower() for i, c in enumerate(s)])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
