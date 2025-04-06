5d11bce91425ecec93d3308ceaedba290d70da27
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2025-04-06 11:15:35 -0400

    static files

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 from flask import Flask, render_template, request, url_for
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == "GET":
        return render_template('index.html')
-    elif request.method =="POST":
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        if username == 'cscholl' and password == 'password':
-            return 'Success'
-        else:
-            return 'Failure'
         
-@app.route('/file_upload', methods=['POST'])
-def file_upload():
-    return 
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
